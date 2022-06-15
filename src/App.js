2f84f97f74acf109d9f15bf6c8d9a0b2ab151369
vishyyvishal <vishalrachapudi1@gmail.com>
2022-06-15 15:00:10 -0500

    week_1 complete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
 
 function App() {
-  return (
-    <div className="App">
-      <NavbarComp/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarComp/>
+        </div>
+    );
 }
 
 export default App;
